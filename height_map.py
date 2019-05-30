1a5350fd24b48f5c73b9a26064e26ddd9b62f917
willy <walbers2@illinois.edu>
2019-05-30 02:31:02 -0500

    almost finished 2nd map

--- height_map.py
+++ height_map.py
@@ -1,52 +1,66 @@
 # Sources
 # http://ncdrisc.org/data-downloads-height.html
 # https://hub.arcgis.com/datasets/a21fdb46d23e4ef896f31475217cbb08_1?geometry=-132.187%2C-88.331%2C227.812%2C88.537
-
+import math
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import geopandas as gpd
 matplotlib.use('Agg')
 
-file = 'data/world_map/Countries_WGS84.shp'
-map_df = gpd.read_file(file)
+men = False
+color = 'RdPu'
 
-print(map_df.head())
+map_df = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
 x = map_df.plot()
-x.figure.savefig('fig1')
-
+# x.figure.savefig('fig1')
 
 df = pd.read_csv('data/mean_height_18_countries.csv', header=0)
-print(df.head())
+df = df[['Country', 'ISO', 'Sex', 'Year of birth', 'Mean height (cm)']]
 
+# get data for men and women
+df_men = df[df.Sex == 'Men']
+df_women = df[df.Sex == 'Women']
 
-df = df[['Country', 'Sex', 'Mean height (cm)']]
-data_for_map = df.rename(index=str, columns={'Mean height (cm)': 'height'})
+# get rid of all years but 1996 (most recent year in data set)
+df_men = df_men[df_men['Year of birth'] == 1996]
+df_women = df_women[df_women['Year of birth'] == 1996]
 
-## !! PULL OUT ROWS HERE !!
+data_for_map_men = df_men.rename(index=str, columns={'Year of birth': 'year', 'Mean height (cm)': 'height'})
+data_for_map_women = df_women.rename(index=str, columns={'Year of birth': 'year', 'Mean height (cm)': 'height'})
 
+print(data_for_map_men)
+print(data_for_map_women)
 
-print(data_for_map.head())
+if (men):
+    merged_men = map_df.set_index('iso_a3').join(data_for_map_men.set_index('ISO'))
+else:
+    merged_women = map_df.set_index('iso_a3').join(data_for_map_women.set_index('ISO'))
 
-#merged = map_df.set_index('CNTRY_NAME').join(data_for_map.set_index('Country'))
-#print(merged.head())
+mapping = 'height'
 
-#mapping = 'height'
+vmin, vmax = 130, 200
+fig, ax = plt.subplots(1, figsize=(10, 6))
 
-'''
-vmin, vmax = 50, 250
-fig, ax = plt.subplots(1, figsize(10, 6))
+if (men):
+    men_map = merged_men.plot(column=mapping, cmap=color, linewidth=.8, ax=ax, edgecolor='.8')
+    men_map.figure.savefig('fig2')
+else:
+    women_map = merged_women.plot(column=mapping, cmap=color, linewidth=.8, ax=ax, edgecolor='.8')
+    women_map.figure.savefig('fig2')
 
-x= merged.plot(column = mapping, cmap='Blues', linewidth=.8, ax=ax, edgecolor=',8')
-x.figure.savefig('fig2')
 
 ax.axis('off')
-ax.set_title('Height (cm) in world')
+if (men):
+    ax.set_title('Height (cm) of men in 1996')
+else:
+    ax.set_title('Height (cm) of women in 1996')
 
-sm = plt.cm.ScalarMappable(cmap='Blues', norm=plt.Normalize(vmin=vmin, vmax=vmax))
+sm = plt.cm.ScalarMappable(cmap=color, norm=plt.Normalize(vmin=vmin, vmax=vmax))
 sm._A = []
 cbar = fig.colorbar(sm)
-
-fig.savefig('map.png',dpi=300)
-'''
-
+if (men):
+    fig.savefig('men_map.png',dpi=300)
+else:
+    fig.savefig('women_map.png',dpi=300)
